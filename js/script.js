b21bda65d9bbadbbe12381935afa54991536e9c1
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-08 17:30:35 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Ain Jeong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: May 2025
+
 // This file contains the JS functions for index.html
+
+"use-strict"
+
+/**
+* This function calculate, doing multiplication
+*/
+// eslint-disable-next-line no-unused-vars
+function calculateNumber () {
+    let answer = 0 
+  //input 
+  const userNumber = parseFloat(document.getElementById('number-one').value)
+
+  //process
+  for (let counter = 1; counter <= userNumber; counter++) {
+    answer += counter
+  }
+
+  // output
+  document.getElementById('answer').innerHTML = 'Your answer is:' + answer + '.'
+}

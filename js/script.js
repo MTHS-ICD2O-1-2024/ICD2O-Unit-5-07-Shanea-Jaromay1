b602254c754eccd23190bb53d797038228b75c83
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-05-09 01:22:54 +0000

    plz

--- js/script.js
+++ js/script.js
@@ -22,9 +22,9 @@ function calculateNumber () {
 
   // output
   let output = ''
-  output += 'Step 1: ' + userNumber + ' / 2 = ' + step1 + '.'
-  output += 'Stpe 2: ' + userNumber + ' + 1 = ' + step2 + '.'
-  output += 'Step 3: ' + step1 + ' * ' + step2 + ' = ' + answer + '.'
-  output += 'Your answer is ' + answer + ' . '
+  output += 'Step 1: ' + userNumber + ' / 2 = ' + step1 + '<br/>'
+  'Stpe 2: ' + userNumber + ' + 1 = ' + step2 + '<br/>'
+  'Step 3: ' + step1 + ' * ' + step2 + ' = ' + answer + '<br/>'
+  'Your answer is ' + answer + ' . '
   document.getElementById('answer').innerHTML = 'Your answer is:' + answer + '.'
 }
